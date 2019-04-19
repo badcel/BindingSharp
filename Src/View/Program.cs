5b8484a1407c83b979e936176525d7b955119fbe
Marcel Tiede <marcel@asterix.lan>
2019-04-19 14:56:55 +0200

    Update

--- Src/View/Program.cs
+++ Src/View/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using Gtk;
 
 namespace MVVM
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Application.Init();
+
+            var app = new Application("org.GTK.GTK", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
+
+            var win = new Window("MEIN FENSTER");
+            app.AddWindow(win);
+
+            win.Show();
+            Application.Run();
         }
     }
 }
