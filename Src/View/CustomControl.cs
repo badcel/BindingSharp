182c715d8a70d22c8656f2026fb131709df7c188
Marcel Tiede <marcel@asterix.lan>
2019-04-27 10:58:07 +0200

    Move logic into binding builder

--- Src/View/CustomControl.cs
+++ Src/View/CustomControl.cs
@@ -7,6 +7,8 @@ namespace MVVM
 {
     public class CustomControl : Box, IView
     {
+        private readonly BindingBuilder builder;
+
         [UI]
         private Button Button;
 
@@ -16,59 +18,16 @@ namespace MVVM
 
         public CustomControl() : this(new BindingBuilder("CustomControl.glade")) { }
 
-        public CustomControl(Builder builder) : base(builder.GetObject("Box").Handle)
+        public CustomControl(BindingBuilder builder) : base(builder.GetObject("Box").Handle)
         {
-            builder.Autoconnect(this);
-
-            Button.Clicked += (o, args) => Label.LabelProp = "FUBAR";
+            this.builder = builder ?? throw new ArgumentNullException(nameof(builder));
         }
 
         public void SetupBindings(object viewModel)
         {
-            var flags = System.Reflection.BindingFlags.Public;
-            flags |= System.Reflection.BindingFlags.NonPublic;
-            flags |= System.Reflection.BindingFlags.DeclaredOnly;
-            flags |= System.Reflection.BindingFlags.Instance;
+            builder.Autoconnect(this, viewModel);
 
-            var fields = GetType().GetFields(flags);
-
-            foreach (var field in fields)
-            {
-                var attrs = field.GetCustomAttributes(typeof(BindingAttribute), false);
-
-                if (attrs == null || attrs.Length == 0)
-                    continue;
-
-                var gtkObject = field.GetValue(this);
-                if (gtkObject is GLib.Object gobject)
-                {
-                    var attr = (BindingAttribute)attrs[0];
-
-                    var t = gobject.GetType();
-                    var p = t.GetProperty(attr.Source);
-                    var pattr = p.GetCustomAttributes(typeof(PropertyAttribute), false);
-
-                    if (pattr == null || pattr.Length == 0)
-                        continue;
-
-                    var src = ((PropertyAttribute)pattr[0]).Name;
-
-                    gobject.AddNotification(src, (o, args) => NotifyViewModel(o, args.Property, viewModel, attr.Target));
-                }
-            }
-        }
-
-        private void NotifyViewModel(object source, string sourceProp, object target, string targetProp)
-        {
-            if (source is GLib.Object gobject)
-            {
-                var value = gobject.GetProperty(sourceProp);
-                if (value is GLib.Value gvalue)
-                {
-                    var prop = target.GetType().GetProperty(targetProp);
-                    prop.SetValue(target, gvalue.Val);
-                }
-            }
+            Button.Clicked += (o, args) => Label.LabelProp = "FUBAR";
         }
     }
 }
