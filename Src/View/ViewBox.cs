272404dfc832f8f7fdbe12785b6719e4171e1579
Marcel Tiede <marcel@localhost.localdomain>
2019-06-15 13:42:59 +0200

    Updates

--- Src/View/ViewBox.cs
+++ Src/View/ViewBox.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using GLib;
 using Gtk;
 
@@ -9,9 +11,16 @@ namespace MVVM
     {
         protected readonly object viewModel;
 
+        private Dictionary<object, List<string>> viewNotifications;
+        private Dictionary<object, List<string>> viewModelNotifications;
+
         public ViewBox(object viewModel)
         {
             this.viewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+
+            viewNotifications = new Dictionary<object, List<string>>();
+            viewModelNotifications = new Dictionary<object, List<string>>();
+
             Bind(this, viewModel);
         }
 
@@ -61,9 +70,45 @@ namespace MVVM
                     
                     var viewProperty = ((PropertyAttribute)bindingViewPropAttr[0]).Name;
                     viewFieldGObject.SetProperty(viewProperty, new GLib.Value(viewModelPropValue));
-                    viewFieldGObject.AddNotification(viewProperty, (o, args) => NotifyViewModel(o, args.Property, viewModel, bindingAttr.ViewModelProperty));
+                    
+                    AddViewNotification(viewFieldGObject, viewProperty, bindingAttr.ViewModelProperty);
+                    AddViewModelNotification();
+
+                    
                 }
             }
+        }
+
+        private void AddViewModelNotification()
+        {
+            if(viewModel is INotifyPropertyChanged notifyPropertyChanged)
+            {
+                notifyPropertyChanged.PropertyChanged += OnPropertyChanged;
+            }
+        }
+
+        private void AddViewNotification(GLib.Object viewFieldGObject, string viewProperty, string viewModelProperty)
+        {
+            //TODO DISPOSE EVENTS!!!
+            viewFieldGObject.AddNotification(viewProperty, (o, args) => NotifyViewModel(o, args.Property, viewModel, viewModelProperty));
+            if(viewNotifications.ContainsKey(viewFieldGObject))
+            {
+                viewNotifications[viewFieldGObject].Add(viewProperty);
+            }
+            else
+            {
+                viewNotifications[viewFieldGObject] = new List<string>(){viewProperty};
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void NotifyView()
+        {
+
         }
 
         private void NotifyViewModel(object source, string sourceProp, object target, string targetProp)
