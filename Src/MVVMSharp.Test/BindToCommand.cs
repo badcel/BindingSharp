400709242a7556d4b4234743c69fee84fe34cf1d
Marcel Tiede <marcel@localhost.localdomain>
2019-06-23 22:38:34 +0200

    Start with unit tests

---
+++ Src/MVVMSharp.Test/BindToCommand.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MVVMSharp.Test.View
+{
+    [TestClass]
+    public class BindToCommand
+    {
+        [TestMethod]
+        public void IsExtensionToGtkWidget()
+        {
+        }
+
+        [TestMethod]
+        public void ReportsErrorIfAttributeDoesNotReferenceICommandInViewModel()
+        {
+        }
+
+        [TestMethod]
+        public void ReportsErrorIfAttributeIsNotAppliedToGtkButtonField()
+        {
+        }
+
+        [TestMethod]
+        public void ForwardsICommandCanExecuteChangedEventToReferencedGtkButtonFieldIsSensitiveProperty()
+        {
+        }
+
+        [TestMethod]
+        public void SetsGtkButtonFieldSensitivePropertyToReferencedICommandCanExecuteMethodDuringBinding()
+        {
+        }
+
+        [TestMethod]
+        public void ForwardGtkButtonFieldClickedEventToReferencedICommandExecuteMethod()
+        {
+        }
+    }
+}
